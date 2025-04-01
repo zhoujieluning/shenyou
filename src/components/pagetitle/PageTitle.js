cea5e4f8d68dd940845c009f89c5a4ab08104f38
ryan <9242762+ryannnnnnnnnn@user.noreply.gitee.com>
2025-04-01 15:28:34 +0800

    挂外链

--- src/components/pagetitle/PageTitle.js
+++ src/components/pagetitle/PageTitle.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Fade from 'react-reveal/Fade'
-import shape1 from '../../images/page-title-shape-1.png'
-import shape2 from '../../images/page-title-shape-2.png'
-import pageTitle from '../../images/page-title.jpg'
+import React from "react";
+import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
+import shape1 from "../../images/page-title-shape-1.png";
+import shape2 from "../../images/page-title-shape-2.png";
+import pageTitle from "../../images/page-title.jpg";
 
 const PageTitle = (props) => {
   return (
     <section className="page-title">
-      <img src={pageTitle} />
+      <a href="http://kh.htsec.com/p/mQjudD" target="_blank">
+        <img src={pageTitle} />
+      </a>
     </section>
-  )
-}
+  );
+};
 
-export default PageTitle
+export default PageTitle;
